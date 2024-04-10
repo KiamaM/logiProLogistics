c3a34a4357060f4442611d19b0cb0fbcddf1754a
unknown <muriithikiamad1@gmail.com>
2024-04-10 12:26:01 +0300

    Added Footer

--- Frontend/logiProFrontend/src/app/app.routes.ts
+++ Frontend/logiProFrontend/src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
-import { NavbarComponent } from './Components/navbar/navbar.component';
+import { LandingPageComponent } from './Components/landing-page/landing-page.component';
 
 export const routes: Routes = [
 
-    {path: '', component:NavbarComponent},
+    {path: '', component:LandingPageComponent},
 
 ];
 
