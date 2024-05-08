b59a6da4293ca6c557bc0abcd5353ca9a6476130
Daniel Muriithi <daniel.muriithi@griffinglobaltech.com>
2024-05-08 12:24:18 +0300

    Added tests for error handling in auth

--- Backend/dist/Controllers/__test__/auth.controller.spec.js
+++ Backend/dist/Controllers/__test__/auth.controller.spec.js
@@ -46,7 +46,7 @@ describe('User authentication test suite', () => {
         jest.spyOn(mssql_1.default, 'connect').mockResolvedValueOnce({
             request: jest.fn().mockReturnThis(),
             input: jest.fn().mockReturnThis(),
-            execute: jest.fn().mockRejectedValueOnce({
+            execute: jest.fn().mockResolvedValueOnce({
                 recordset: [expectedUser]
             })
         });
@@ -56,6 +56,59 @@ describe('User authentication test suite', () => {
         expect(res.json).toHaveBeenCalledWith({
             message: 'Login success',
             token: 'generated-tokenqwertyhjs21345twfscvdfd'
+        });
+    }));
+    //Test case for wrong email address
+    test('It issues an error for wrong email', () => __awaiter(void 0, void 0, void 0, function* () {
+        const req = {
+            body: {
+                email: 'Wrongemail@mail.com',
+                password: 'password'
+            }
+        };
+        jest.spyOn(mssql_1.default, 'connect').mockResolvedValueOnce({
+            request: jest.fn().mockReturnThis,
+            input: jest.fn().mockReturnThis(),
+            execute: jest.fn().mockResolvedValueOnce({
+                recordset: []
+            })
+        });
+        yield (0, auth_controller_1.loginUser)(req, res);
+        expect(res.json).toHaveBeenCalledWith({
+            error: 'User not found'
+        });
+    }));
+    //Test case for wrong password
+    test('It issues an eror when the password is wrong', () => __awaiter(void 0, void 0, void 0, function* () {
+        let expectedUser = {
+            userId: "85d9b39d-75b6-4496-a9ed-39960469606d",
+            firstName: "John",
+            lastName: "Doe",
+            email: "muriithikiamad1+456@gmail.com",
+            role: "customer",
+            phoneNumber: "03655644567",
+            password: "$2b$05$QH29qQBf.HtOM1x8m6GE..rTfd631CyK.v5kAuNzBRkt5KOgeoszK",
+            isWelcomed: false,
+            isDeleted: false,
+            registerDate: "2024-05-07T00:00:00.000Z"
+        };
+        const req = {
+            body: {
+                email: expectedUser.email,
+                password: 'wrongPassword'
+            }
+        };
+        jest.spyOn(mssql_1.default, 'connect').mockResolvedValueOnce({
+            request: jest.fn().mockReturnThis(),
+            input: jest.fn().mockReturnThis(),
+            execute: jest.fn().mockResolvedValueOnce({
+                recordset: [expectedUser]
+            })
+        });
+        jest.spyOn(bcrypt_1.default, 'compare').mockResolvedValueOnce(false);
+        yield (0, auth_controller_1.loginUser)(req, res);
+        expect(res.json).toHaveBeenCalledWith({
+            error: 'Incorrect password'
         });
     }));
 });
